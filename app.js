fe7a196876748c07fe5de4134715ec4e483d7aab
Caroli-ne <karoline_gnn@hotmail.com>
2023-09-18 19:35:22 -0300

    refactor: Adiciona princípios solid

--- app.js
+++ app.js
@@ -2,18 +2,28 @@ var prompt = require('prompt-sync')();
 
 const lista = [];
 
-function listaOrdenada () {
-    var entrada = prompt ("Insira o texto, ou SAIR para encerrar: ");
-    if (entrada.toUpperCase()  === 'SAIR') {
-        lista.sort()
-        console.log('Propriedades inseridas:')
-        lista.forEach(item => {
-            console.log (item)
-        })
-    }else{
-        lista.push(entrada.toLowerCase())
-        listaOrdenada()
+function obterEntrada() {
+    return prompt("Insira o texto, ou SAIR para encerrar: ");
+}
+
+function exibirListaOrdenada(lista) {
+    lista.sort();
+    console.log('Propriedades inseridas:');
+    lista.forEach(item => {
+        console.log(item);
+    });
+}
+
+function main() {
+    while (true) {
+        const entrada = obterEntrada().toLowerCase();
+        if (entrada === 'sair') {
+            exibirListaOrdenada(lista);
+            break;
+        } else {
+            lista.push(entrada);
+        }
     }
 }
 
-listaOrdenada()
+main();
